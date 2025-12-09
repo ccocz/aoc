f04cb588f5e3727129944227206291c3471552f1
Resul Hangeldiyev <22514676+ccocz@users.noreply.github.com>
2025-12-09 14:46:15 +0000

    Add files via upload

--- 2025/day9.cpp
+++ 2025/day9.cpp
@@ -1,33 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
 int main() {
 	string s;
-	
 	vector<pair<int, int>> v;
 	
 	while (cin >> s) {
 		stringstream ss(s);
-		
 		int a, b;
 		char c;
-		
 		ss >> a >> c >> b;
 		
 		v.push_back({b, a});
 	}
 	
 	int n = v.size();
-	
-	/*long long area = 0;
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = i + 1; j < n; j++) {
-			area = max(area, 1LL * (abs(v[i].first - v[j].first) + 1) * (abs(v[i].second - v[j].second) + 1));
-		}
-	}*/
-	
+		
 	vector<pair<int, pair<int, int>>> hor, ver;
 	
 	for (int i = 0; i < n; i++) {
@@ -41,61 +32,95 @@ int main() {
 	
 	sort(hor.begin(), hor.end());
 	sort(ver.begin(), ver.end());
-	
-	for (auto& jal : hor) {
-		cout << jal.second.first << " xx " << jal.second.second << endl; 
-	}
-	
+
 	long long area = 0;
 	
 	for (int i = 0; i < n; i++) {
-		for (int j = i; j < n; j++) {
+		for (int j = i + 1; j < n; j++) {
 			int mini_r = min(v[i].first, v[j].first);
 			int maxi_r = max(v[i].first, v[j].first);
 			
 			int mini_c = min(v[i].second, v[j].second);
 			int maxi_c = max(v[i].second, v[j].second);
 			
-			pair<int, int> horr = hor[0].second;
-			int hi = 1;
+			vector<pair<int, int>> jul;
+
+			int hi = 0;
 			while (hi < hor.size() && hor[hi].first <= mini_r) {
-				horr = {min(horr.first, hor[hi].second.first), max(horr.second, hor[hi].second.second)};
-				
+				if (max(hor[hi].second.first, mini_c) <= min(hor[hi].second.second, maxi_c)) jul.push_back(hor[hi].second);
 				hi++;
 			}
-			
-			if (!(mini_c >= horr.first && maxi_c <= horr.second)) continue;
-			
-			horr = hor[hor.size() - 1].second;
-			hi = hor.size() - 2;
+
+			sort(jul.begin(), jul.end());
+			if (jul.empty() || !(mini_c >= jul[0].first && maxi_c <= jul[jul.size() - 1].second)) continue;
+
+			bool ok = true;
+			int maxx = jul[0].second;
+			for (int ii = 1; ii < jul.size(); ii++) {
+				ok &= jul[ii].first <= maxx;
+				maxx = max(maxx, jul[ii].second);
+			}
+
+			if (!ok) continue;
+			jul.clear();
+
+			hi = hor.size() - 1;
 			while (hi >= 0 && hor[hi].first >= maxi_r) {
-				horr = {min(horr.first, hor[hi].second.first), max(horr.second, hor[hi].second.second)};
+				if (max(hor[hi].second.first, mini_c) <= min(hor[hi].second.second, maxi_c)) jul.push_back(hor[hi].second);
 				hi--;
 			}
+
+			sort(jul.begin(), jul.end());
+			if (jul.empty() || !(mini_c >= jul[0].first && maxi_c <= jul[jul.size() - 1].second)) continue;
+
+			ok = true;
+			maxx = jul[0].second;
+			for (int ii = 1; ii < jul.size(); ii++) {
+				ok &= jul[ii].first <= maxx;
+				maxx = max(maxx, jul[ii].second);
+			}
+
+			if (!ok) continue;
+			jul.clear();
 			
-			if (!(mini_c >= horr.first && maxi_c <= horr.second)) continue;
-			
-			pair<int, int> verr = ver[0].second;
-			hi = 1;
+			hi = 0;
 			while (hi < ver.size() && ver[hi].first <= mini_c) {
-				verr = {min(verr.first, ver[hi].second.first), max(verr.second, ver[hi].second.second)};
+				if (max(ver[hi].second.first, mini_r) <= min(ver[hi].second.second, maxi_r)) jul.push_back(ver[hi].second);
 				hi++;
 			}
-			
-			if (!(mini_r >= verr.first && maxi_r <= verr.second)) continue;
-			
-			verr = ver[ver.size() - 1].second;
-			hi = ver.size() - 2;
+
+			sort(jul.begin(), jul.end());
+			if (jul.empty() || !(mini_r >= jul[0].first && maxi_r <= jul[jul.size() - 1].second)) continue;
+
+			ok = true;
+			maxx = jul[0].second;
+			for (int ii = 1; ii < jul.size(); ii++) {
+				ok &= jul[ii].first <= maxx;
+				maxx = max(maxx, jul[ii].second);
+			}
+						
+			if (!ok) continue;
+			jul.clear();
+			hi = ver.size() - 1;
 			while (hi >= 0 && ver[hi].first >= maxi_c) {
-				verr = {min(verr.first, ver[hi].second.first), max(verr.second, ver[hi].second.second)};
+				if (max(ver[hi].second.first, mini_r) <= min(ver[hi].second.second, maxi_r)) jul.push_back(ver[hi].second);
 				hi--;
 			}
 			
-			if (!(mini_r >= verr.first && maxi_r <= verr.second)) continue;
+			sort(jul.begin(), jul.end());
+			if (jul.empty() || !(mini_r >= jul[0].first && maxi_r <= jul[jul.size() - 1].second)) continue;
+
+			ok = true;
+			maxx = jul[0].second;
+			for (int ii = 1; ii < jul.size(); ii++) {
+				ok &= jul[ii].first <= maxx;
+				maxx = max(maxx, jul[ii].second);
+			}
+
+			if (!ok) continue;
+			jul.clear();
 			
 			area = max(area, 1LL * (abs(v[i].first - v[j].first) + 1) * (abs(v[i].second - v[j].second) + 1));
-			
-			// cout << v[i].first << "x" << v[i].second << " z " << v[j].first << "x" << v[j].second << " " << 1LL * (abs(v[i].first - v[j].first) + 1) * (abs(v[i].second - v[j].second) + 1) << endl;
 		}
 	}	
 	
